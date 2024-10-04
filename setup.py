2b2d52364e6ca9b7422b5e378782ae27f5b68ac2
abbiereid <abbiereid222@gmail.com>
2024-10-04 15:28:23 +0100

    Add setup.py file

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='BSL_Translator',
+    version='1.0.0',
+    packages=find_packages(),
+    install_requires=[
+        'keras~=3.0.5',
+        'tensorflow~=2.13.0',
+        'opencv-python~=4.9.0.80',
+        'mediapipe~=0.10.9',
+        'scikit-learn~=1.4.0'
+    ],
+)
